f12615da33b2645d1c615465b3680bed8213618d
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-08-14 01:57:38 +0400

    test sw

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => {
     return {
@@ -15,6 +16,15 @@ export default defineConfig(() => {
             assetsDir: ".",
         },
 
-        plugins: [react(), eslint()],
+        plugins: [
+            react(),
+            eslint(),
+            VitePWA({
+                registerType: "autoUpdate",
+                strategies: "injectManifest",
+                srcDir: "src",
+                filename: "sw.js",
+            }),
+        ],
     };
 });
