f2cd14130df3a9823dba03822111c9a847192e8c
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-10 14:59:12 +0400

    test css

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Home from "./Home.tsx";
 import NotFoundPage from "./NotFoundPage.tsx";
 
 export default function App() {
-    console.log("test 9");
+    console.log("test 10");
     return (
-        <main className="h-screen bg-black text-white">
+        <main className="h-screen bg-black text-gray-300">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="*" element={<NotFoundPage />} />
