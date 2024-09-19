5aa00832f942ad6289148adfe1dfff09168e84cf
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-19 22:32:20 +0400

    changed to BrowserRouter instead of HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <HashRouter>
+        <Router basename="/react-vite-template">
             <App />
-        </HashRouter>
+        </Router>
     </React.StrictMode>
 );
