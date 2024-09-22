d0e776f9487b4961eca4426d7589b6fd6f14a98e
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-22 22:09:41 +0400

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-vite-template/service-worker.js', { scope: '/react-vite-template/' })})}
