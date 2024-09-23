e4c40b63fbe1189f4d122c9ef52fbf75035a0a5a
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-23 23:40:40 +0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,101 @@
+import { registerRoute } from "../node_modules/workbox-routing";
+import { NetworkFirst, StaleWhileRevalidate, CacheFirst } from "../node_modules/workbox-strategies";
+import { CacheableResponsePlugin } from "../node_modules/workbox-cacheable-response";
+import { ExpirationPlugin } from "../node_modules/workbox-expiration";
+import { Workbox } from "../node_modules/workbox-window";
+
+registerRoute(
+    ({ request }) => request.mode === "navigate",
+    new NetworkFirst({
+        cacheName: "pages",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ request }) =>
+        request.destination === "style" || request.destination === "script" || request.destination === "worker",
+    new StaleWhileRevalidate({
+        cacheName: "assets",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ request }) => request.destination === "image",
+    new CacheFirst({
+        cacheName: "images",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ url }) => url.origin === "https://fonts.googleapis.com",
+    new StaleWhileRevalidate({
+        cacheName: "google-fonts-stylesheets",
+    })
+);
+
+registerRoute(
+    ({ url }) => url.origin === "https://fonts.gstatic.com",
+    new CacheFirst({
+        cacheName: "google-fonts-webfonts",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ url }) => url.origin === "https://hacker-news.firebaseio.com",
+    new NetworkFirst({
+        networkTimeoutSeconds: 3,
+        cacheName: "stories",
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 50,
+                maxAgeSeconds: 5 * 60, // 5 minutes
+            }),
+        ],
+    })
+);
+
+if ("serviceWorker" in navigator) {
+    const workbox = new Workbox(`${import.meta.env.VITE_PUBLIC_URL}/service-worker.js`);
+
+    workbox.addEventListener("installed", (event) => {
+        if (!event.isUpdate) {
+            console.log("Service worker installed: ", event);
+        }
+    });
+
+    workbox.addEventListener("activated", (event) => {
+        if (!event.isUpdate) {
+            console.log("Service worker activated for the first time!");
+        }
+    });
+
+    workbox.register();
+}
