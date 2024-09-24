b1f46ed5c4edcf33806a6d1c3ff482f76116a097
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-24 15:00:17 +0400

    test sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,5 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js");
 
-const CACHE_NAMES = {
-    pages: "pages",
-    assets: "assets",
-    images: "images",
-};
-
-const CURRENT_CACHE_VERSION = "v1";
-
 // Skip waiting during the install phase
 self.addEventListener("install", () => {
     self.skipWaiting();
@@ -15,38 +7,23 @@ self.addEventListener("install", () => {
 
 // Claim control over the clients in the activate phase
 self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        // Get all cache keys and delete those that don't match the current version
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames.map((cacheName) => {
-                        function getCacheNamesToDelete() {
-                            return Object.values(CACHE_NAMES).map((cache) => `${cache}-${CURRENT_CACHE_VERSION}`);
-                        }
-
-                        // Delete old caches if they don't match the current cache version
-                        if (!getCacheNamesToDelete().includes(cacheName)) {
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-            })
-            .then(() => {
-                return self.clients.claim();
-            })
-    );
+    event.waitUntil(self.clients.claim());
 });
+
+// Precaching assets (you can automate this during the build process)
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
+// Cleanup outdated caches
+workbox.precaching.cleanupOutdatedCaches();
 
 // Cache pages with a NetworkFirst strategy
 workbox.routing.registerRoute(
     ({ request }) => request.mode === "navigate",
     new workbox.strategies.NetworkFirst({
-        cacheName: `${CACHE_NAMES.pages}-${CURRENT_CACHE_VERSION}`,
+        cacheName: "pages",
         plugins: [
             new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200],
+                statuses: [200],
             }),
         ],
     })
@@ -57,10 +34,10 @@ workbox.routing.registerRoute(
     ({ request }) =>
         request.destination === "style" || request.destination === "script" || request.destination === "worker",
     new workbox.strategies.StaleWhileRevalidate({
-        cacheName: `${CACHE_NAMES.assets}-${CURRENT_CACHE_VERSION}`,
+        cacheName: "assets",
         plugins: [
             new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200],
+                statuses: [200],
             }),
         ],
     })
@@ -70,10 +47,10 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
     ({ request }) => request.destination === "image",
     new workbox.strategies.CacheFirst({
-        cacheName: `${CACHE_NAMES.images}-${CURRENT_CACHE_VERSION}`,
+        cacheName: "images",
         plugins: [
             new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200],
+                statuses: [200],
             }),
             new workbox.expiration.ExpirationPlugin({
                 maxEntries: 50,
