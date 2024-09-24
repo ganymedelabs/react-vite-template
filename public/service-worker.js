acae7e2fd7144c887bf8ac4382c911a22f70d3ce
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-24 17:31:09 +0400

    test sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,28 +20,25 @@ workbox.routing.registerRoute(
     })
 );
 
-const cacheAndUpdate = async ({ request }) => {
+async function cacheAndUpdate({ request }) {
     const cache = await caches.open("assets");
     const cachedResponse = await cache.match(request);
     const networkResponse = await fetch(request);
 
     if (networkResponse && networkResponse.status === 200) {
-        // Store the new version in the cache
         await cache.put(request, networkResponse.clone());
 
-        // Clean up old cache entries that match the filename pattern but have a different hash
         const cacheKeys = await cache.keys();
         const requestUrl = new URL(request.url);
-        const fileNamePattern = requestUrl.pathname.split("/").pop().split(".")[0]; // "index" in index-[hash].js
-        console.log({ cacheKeys, requestUrl, fileNamePattern });
 
         for (const cacheKey of cacheKeys) {
             const cachedUrl = new URL(cacheKey.url);
-            console.log({ cacheKey, cachedUrl });
 
-            // Match files with the same base name but different hashes
             if (
-                cachedUrl.href !== requestUrl.href // Check if it's a different hash
+                cachedUrl.pathname.split(".").at(-1) === requestUrl.pathname.split(".").at(-1) &&
+                cachedUrl.pathname.includes("index") &&
+                requestUrl.pathname.includes("index") &&
+                cachedUrl.href !== requestUrl.href
             ) {
                 await cache.delete(cacheKey);
                 console.log(`Deleted outdated file from cache: ${cachedUrl.href}`);
@@ -52,11 +49,9 @@ const cacheAndUpdate = async ({ request }) => {
         return networkResponse;
     }
 
-    // Return the cached version if available and network response failed
     return cachedResponse || networkResponse;
-};
+}
 
-// Register the route for scripts, styles, and worker files with the custom handler
 workbox.routing.registerRoute(
     ({ request }) =>
         request.destination === "style" || request.destination === "script" || request.destination === "worker",
