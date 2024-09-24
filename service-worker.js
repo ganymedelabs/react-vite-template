92d87610e29357f8002a27ae907f62e71d3035b4
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-24 15:05:00 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -10,52 +10,52 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim());
 });
 
-// Precaching assets (you can automate this during the build process)
+// Precaching assets
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
 
 // Cleanup outdated caches
 workbox.precaching.cleanupOutdatedCaches();
 
 // Cache pages with a NetworkFirst strategy
-workbox.routing.registerRoute(
-    ({ request }) => request.mode === "navigate",
-    new workbox.strategies.NetworkFirst({
-        cacheName: "pages",
-        plugins: [
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [200],
-            }),
-        ],
-    })
-);
+// workbox.routing.registerRoute(
+//     ({ request }) => request.mode === "navigate",
+//     new workbox.strategies.NetworkFirst({
+//         cacheName: "pages",
+//         plugins: [
+//             new workbox.cacheableResponse.CacheableResponsePlugin({
+//                 statuses: [200],
+//             }),
+//         ],
+//     })
+// );
 
-// Cache assets (CSS, JS, etc.) with a StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-    ({ request }) =>
-        request.destination === "style" || request.destination === "script" || request.destination === "worker",
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: "assets",
-        plugins: [
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [200],
-            }),
-        ],
-    })
-);
+// // Cache assets (CSS, JS, etc.) with a StaleWhileRevalidate strategy
+// workbox.routing.registerRoute(
+//     ({ request }) =>
+//         request.destination === "style" || request.destination === "script" || request.destination === "worker",
+//     new workbox.strategies.StaleWhileRevalidate({
+//         cacheName: "assets",
+//         plugins: [
+//             new workbox.cacheableResponse.CacheableResponsePlugin({
+//                 statuses: [200],
+//             }),
+//         ],
+//     })
+// );
 
-// Cache images with a CacheFirst strategy and expiration control
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === "image",
-    new workbox.strategies.CacheFirst({
-        cacheName: "images",
-        plugins: [
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [200],
-            }),
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-            }),
-        ],
-    })
-);
+// // Cache images with a CacheFirst strategy and expiration control
+// workbox.routing.registerRoute(
+//     ({ request }) => request.destination === "image",
+//     new workbox.strategies.CacheFirst({
+//         cacheName: "images",
+//         plugins: [
+//             new workbox.cacheableResponse.CacheableResponsePlugin({
+//                 statuses: [200],
+//             }),
+//             new workbox.expiration.ExpirationPlugin({
+//                 maxEntries: 50,
+//                 maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+//             }),
+//         ],
+//     })
+// );
