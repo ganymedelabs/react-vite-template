8a309a93b3ee67eafedd5a75b52d3cda2a5bf958
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-25 15:00:03 +0400

    fixed router

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Router basename="/react-vite-template/">
+        <Router basename="/react-vite-template">
             <App />
         </Router>
     </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(() => {
             port: 3000,
         },
 
-        base: "/react-vite-template/",
+        base: "/react-vite-template",
 
         build: {
             outDir: "build",
