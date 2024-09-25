e9da5529b8bd1e66257a2e03675c440f909e72ed
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-25 15:54:00 +0400

    test router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { useEffect } from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 
 export default function App() {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const currentUrl = location.search;
-
-        if (currentUrl.startsWith("/?/")) {
-            const extractedPath = currentUrl.replace("/?/", "");
-
-            navigate(`/${extractedPath}`, { replace: true });
-        }
-    }, [location, navigate]);
-
     return (
         <main className="h-screen bg-black text-white">
             <Routes>
