d71c7e972bb4947df4f9217ea4dcbc791d35ff2e
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-25 21:35:30 +0400

    test router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom";
 
 export default function App() {
-    console.log("test 8");
+    console.log("test 9");
     return (
-        <main className="h-screen bg-black text-green-100">
+        <main className="h-screen bg-black text-yellow-100">
             <Outlet />
         </main>
     );
